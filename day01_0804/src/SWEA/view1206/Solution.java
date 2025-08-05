0c995e1f87838b1591c1a7181b3117a7be86272e
blublunv <blublunv@gmail.com>
2025-08-05 16:04:59 +0900

    asdf

---
+++ day01_0804/src/SWEA/view1206/Solution.java
@@ -0,0 +1,15 @@
+package SWEA.view1206;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		//File file = new File("./src/SWEA/view1206/sample_input.txt");
+		//Scanner sc = new Scanner(file);
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
