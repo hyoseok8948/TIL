78f4126d6376448b2b0774e3c86a32f19cd280dc
blublunv <blublunv@gmail.com>
2025-08-05 17:53:43 +0900

    ssss

---
+++ day02_0805/src/day02_SWEA/Follwing2.java
@@ -0,0 +1,71 @@
+package day02_SWEA;
+
+import java.util.Scanner;
+
+public class Follwing2 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int testCase = sc.nextInt();
+	
+	//크기 입력 > 배열에 생성&크기 할당 > 이동할 칸의 수(K)와 배열이동에 필요한 D선언
+	//좌표 구하기 > 숫자값에 쓸 num선언 >
+	for(int tc = 0; tc <= testCase; tc++) {//이게 끝까지간다
+		int N = sc.nextInt();//크기입력
+		int[][] arr = new int[N][N];//크기할당
+		
+		int K = N; //이동할 칸의 수
+		int D = 1; //배열에 더해지는 수 -1로 반전(빙글빙글이 가능하다.)
+		int num = 1; //값에 더해지는 수
+		
+		
+		//현재좌표
+		int r = 0;//얘는 1차 입력에서 고정이니까 0부터 시작한다.
+		int c = -1;//얘는 맨처음에 D값을 더하면서 시작하니까 -1부터 시작해준다.
+		
+		while(true) {
+			for(int i =0; i < K; i++) {
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ day02_0805/src/day02_SWEA/SWEA_달팽이.java
@@ -0,0 +1,24 @@
+package day02_SWEA;
+
+public class SWEA_달팽이 {
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
